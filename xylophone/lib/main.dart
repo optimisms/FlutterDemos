70b5c5e11d844bc4f03b7879cb67bc7c2136337b
optimisms <optimisms@users.noreply.github.com>
2022-11-11 09:18:52 -0700

    Added sound functionality

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,19 +4,88 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playNote(int soundNum) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNum.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('');
-              },
-              child: const Text('Click Me'),
-            ),
+          child: Row(
+            children: <Widget>[
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                ),
+                onPressed: () {
+                  playNote(1);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.orange),
+                ),
+                onPressed: () {
+                  playNote(2);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.yellow),
+                ),
+                onPressed: () {
+                  playNote(3);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                ),
+                onPressed: () {
+                  playNote(4);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.lightBlue),
+                ),
+                onPressed: () {
+                  playNote(5);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () {
+                  playNote(6);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple),
+                ),
+                onPressed: () {
+                  playNote(7);
+                },
+                child: const Text(''),
+              ),
+            ],
           ),
         ),
       ),

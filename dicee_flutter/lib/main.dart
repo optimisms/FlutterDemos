b06d8e138b685929f5908eb5d38f48a2063b9825
optimisms <optimisms@users.noreply.github.com>
2022-11-10 20:37:38 -0700

    Intention actions - added and reformated images and surrounding layouts

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  return runApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -18,13 +11,30 @@ class MyApp extends StatelessWidget {
         ),
         body: DicePage(),
       ),
-    );
-  }
+    ),
+  );
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
